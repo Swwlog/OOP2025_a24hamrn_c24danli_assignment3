32a043a436ef5fd5307956dbe6e55e96558f2ac5
Brukare <Brukare@DESKTOP-VNL6QRN.lan>
2025-02-25 16:02:01 +0100

    prov

---
+++ OOP2025_a24hamrn_c24danli_assignment3/src/assignment3/Worker.java
@@ -0,0 +1,12 @@
+package assignment3;
+
+public class Worker {
+	
+	private String name;
+	private int energy;
+	
+	public Worker () {
+		this.name = name;
+	}
+
+}
---
+++ OOP2025_a24hamrn_c24danli_assignment3/src/module-info.java
@@ -0,0 +1,2 @@
+module OOP2025_a24hamrn_c24danli_assignment3 {
+}
