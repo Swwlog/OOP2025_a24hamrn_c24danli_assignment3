795cce82d180b319057c08f3583739a2b425ee91
Brukare <Brukare@DESKTOP-VNL6QRN.lan>
2025-02-25 17:21:03 +0100

    worker class made

--- OOP2025_a24hamrn_c24danli_assignment3/src/assignment3/HotDrink.java
+++ OOP2025_a24hamrn_c24danli_assignment3/src/assignment3/HotDrink.java
@@ -5,38 +5,38 @@ public class HotDrink {
 	String name;
 
 	public HotDrink(int Coffe) {
-		//this.Name = name;
-		
-		switch(Coffe) {
-			
+		// this.Name = name;
+
+		switch (Coffe) {
+
 		case 1:
-			this.energy = (int) (Math.random() * (20-15)) + 15;
+			this.energy = (int) (Math.random() * (20 - 15)) + 15;
 			this.name = "black coffe";
 			break;
-			
+
 		case 2:
-			this.energy = (int) (Math.random() * (30-20)) + 20;
+			this.energy = (int) (Math.random() * (30 - 20)) + 20;
 			this.name = "Cappuccino";
-			//System.out.println("Made a Cappuccino whit " + this.energy + " energy level");
-		break;
-		
-		
+			// System.out.println("Made a Cappuccino whit " + this.energy + " energy
+			// level");
+			break;
+
 		case 3:
-			this.energy = (int) (Math.random() * (35-25)) + 25;
+			this.energy = (int) (Math.random() * (35 - 25)) + 25;
 			this.name = "Late";
-			//System.out.println("Made a Latte whit " + this.energy + " energy level");
-		break;
+			// System.out.println("Made a Latte whit " + this.energy + " energy level");
+			break;
 		}
-		
+
 		System.out.println("Made a " + this.name + " whit " + this.energy + " energy level");
 	}
-	
+
 	public String GetCoffeName() {
 		return this.name;
 	}
-	
+
 	public int getEnergy() {
 		return this.energy;
 	}
-	
+
 }
--- OOP2025_a24hamrn_c24danli_assignment3/src/assignment3/Worker.java
+++ OOP2025_a24hamrn_c24danli_assignment3/src/assignment3/Worker.java
@@ -1,14 +1,31 @@
 package assignment3;
 
-import java.lang.reflect.Array;
-
 public class Worker {
 
 	private String name;
 	private int energy;
+	private final int energyDepletionTime;
 
-	public Worker() {
+	public Worker(String name) {
+		this.name = name;
+		this.energy =(int)Math.random()*(91-30)+30;
+		this.energyDepletionTime = (int)((Math.random()*(1501-500))+500);
 		
 	}
-
+	public int ShowEnergy() {
+		return energy;
+	}
+	public int DrinkCoffe(int drink) {
+		energy= energy+drink;
+		return energy;
+	}
+	public String GetName() {
+		return name;
+	}
+	public void decreceEnery() {
+		energy = energy - 1;
+	}
+	public int GetEnergyDepleationTime() {
+		return energyDepletionTime;
+	}
 }
