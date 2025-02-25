1442b0d95969f4d9345522fff64845166a350db0
hampu <hampu@10.111.35.79>
2025-02-25 17:20:05 +0100

    Made HotDrink class

--- OOP2025_a24hamrn_c24danli_assignment3/src/assignment3/Fika.java
+++ OOP2025_a24hamrn_c24danli_assignment3/src/assignment3/Fika.java
@@ -4,7 +4,9 @@ public class Fika {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int rand =(int) (Math.random() * (4-1)) + 1;
+		HotDrink hotdrink = new HotDrink(rand);
+		
 	}
 
 }
