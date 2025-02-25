36ebee4ee51799b38dc9daa3ec2502f1044c128f
Brukare <Brukare@DESKTOP-VNL6QRN.lan>
2025-02-25 16:29:50 +0100

    Classes

--- OOP2025_a24hamrn_c24danli_assignment3/src/assignment3/Worker.java
+++ OOP2025_a24hamrn_c24danli_assignment3/src/assignment3/Worker.java
@@ -3,13 +3,12 @@ package assignment3;
 import java.lang.reflect.Array;
 
 public class Worker {
-	
+
 	private String name;
 	private int energy;
-	
-	
-	public Worker () {
-		this.name = name;
+
+	public Worker() {
+		
 	}
 
 }
